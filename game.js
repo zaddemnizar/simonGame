0c7ed7a6cb68cbd6fe963197ba867f67da3be14b
Nizar <79638878+zaddemnizar@users.noreply.github.com>
2021-10-21 23:58:45 +0100

    Final upload

--- game.js
+++ game.js
@@ -2,20 +2,16 @@ var userClickedPattern = [];
 var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 var level = 0;
+var started = false;
 
-//function that returns a random number between 0-3
-function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 4);
-  return randomNumber;
-}
+$(document).keydown(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
 
-//add a random color to gamePatter array
-var randomChosenColour = buttonColours[nextSequence()];
-gamePattern.push(randomChosenColour);
-
-//animate a flash to the button selected
-$("." + randomChosenColour).fadeOut(100).fadeIn(100);
-playSound(randomChosenColour);
+});
 
 //add color of the button clicked to userChosenColour array
 $(".btn").on("click", function() {
@@ -23,7 +19,25 @@ $(".btn").on("click", function() {
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
   animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
 })
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+
+  //generate random number between 0-3
+  var randomNumber = Math.floor(Math.random() * 4);
+
+  //add a random color to gamePatter array
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  //animate a flash to the button selected
+  $("." + randomChosenColour).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
 
 function animatePress(currentColour) {
   $("." + currentColour).addClass("pressed");
@@ -36,4 +50,30 @@ function playSound(name) {
   var audioFile = 'sounds/' + name + '.mp3';
   var audio = new Audio(audioFile);
   audio.play();
+}
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
